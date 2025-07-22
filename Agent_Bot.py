ff0436678631e250352735d661034d90f24d878e
Vivek Gattadi <gattadivivek@gmail.com>
2025-07-22 14:18:23 +0530

    Simple Chatbot No Memory

---
+++ Agent_Bot.py
@@ -0,0 +1,30 @@
+from typing import TypedDict, List
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    messages: List[HumanMessage]
+
+llm = ChatOpenAI(model="gpt-4o")
+
+def process(state: AgentState) -> AgentState:
+    response = llm.invoke(state["messages"])
+    print(f"\nAI: {response.content}")
+    #print(type(response), type(response.content))
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("process", process)
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
+
+agent = graph.compile()
+
+user_input = input("Enter: ")
+while user_input != "exit":
+    agent.invoke({"messages": [HumanMessage(content=user_input)]})
+    user_input = input("Enter: ")
